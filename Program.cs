5f374b6e37c2c24ba1ef8aa6241f303e4d9cc079
Andrei Alechin <142342284+AndreiAlechin@users.noreply.github.com>
2023-09-06 21:27:15 +0100

    First rough draft

--- Program.cs
+++ Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Get command-line arguments
+            int arg1 = int.Parse(args[0]);
+            int arg2 = int.Parse(args[1]);
+            int arg3 = int.Parse(args[2]);
+            char arg4 = char.Parse(args[3]);
+
+            //Run the roller with the arguments and get the result.
+            // Note to self for later: tuple types for getting more detail out of the roller for fancier output?.
+            string rolleroutput = Roller(arg1, arg2, arg3, arg4);
+
+            //Send the roller result to the output method.
+            Outputter(rolleroutput);
+        }
+
+        // Do the dice rolling here.
+        // Note to self for later: tuple types for getting more detail out of the roller for fancier output?.
+        private static string Roller(int numdice, int dicefaces, int modifier, char advordis)
+        {
+            // For testing only. Actual code for the roller goes here.
+            string rollresult = numdice + " " + dicefaces + " " + modifier + " " + advordis;
+            return rollresult;
+        }
+
+        // Output the results.
+        // Basic output for now; might get fancier later.
+        private static void Outputter(string rollermessage)
+        {
+            Console.WriteLine(rollermessage);
+            return;
         }
     }
 }
