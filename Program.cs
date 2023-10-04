f49048d70510fb19ef1db85dfb228d98eac4a3a1
Andrei Alechin <142342284+AndreiAlechin@users.noreply.github.com>
2023-10-04 22:25:39 +0100

    Changed roller code to allow showing both rolls for advantage/disadvantage.

--- Program.cs
+++ Program.cs
@@ -14,8 +14,9 @@ namespace DiceOTron
 
             // Run the roller with the arguments and get the result.
             // Note to self for later: tuple types for getting more detail out of the roller for fancier output?.
+            // Plan B!: int array return type instead?
             // Note to self for later: input checking method to catch bad/incorrect input.
-            string rolleroutput = Roller(arg1, arg2, arg3, arg4);
+            int[] rolleroutput = Roller(arg1, arg2, arg3, arg4);
 
             //Send the roller result to the output method.
             Outputter(rolleroutput);
@@ -23,9 +24,11 @@ namespace DiceOTron
 
         // Do the dice rolling here.
         // Note to self for later: tuple types for getting more detail out of the roller for fancier output?.
-        private static string Roller(int numdice, int dicefaces, int modifier, char advordis)
+        // Plan B!: int array return type instead?
+        // private static (string, string, char) Roller(int numdice, int dicefaces, int modifier, char advordis)
+        private static int[] Roller(int numdice, int dicefaces, int modifier, char advordis)
         {
-            string rollresult;
+            int[] rollresult = new int[3];
             
             switch(advordis)
             {
@@ -34,52 +37,59 @@ namespace DiceOTron
                     // Roll twice.
                     int diceresult_a = InternalRoller(numdice, dicefaces, modifier);
                     int diceresult_b = InternalRoller(numdice, dicefaces, modifier);
+                    // Put each roll into the first two elements of the array.
+                    rollresult[0] = diceresult_a;
+                    rollresult[1] = diceresult_b;
 
-                    // Select the higher roll and return that. If neither is higher, return _a.
+                    // Find the higher roll, set third array element to denote desired roll; default to first roll if equal.
                     if (diceresult_a > diceresult_b)
                     {
-                        rollresult = diceresult_a.ToString();
+                        rollresult[2] = 0;
                         return rollresult;
                     }
                     else if (diceresult_b > diceresult_a)
                     {
-                        rollresult = diceresult_b.ToString();
+                        rollresult[2] = 1;
                         return rollresult;
                     }
                     else
                     {
-                        rollresult = diceresult_a.ToString();
+                        rollresult[2] = 0;
                         return rollresult;
                     }
                 }
                 case 'd':
                 {
-                    //Roll twice.
+                    // Roll twice.
                     int diceresult_a = InternalRoller(numdice, dicefaces, modifier);
                     int diceresult_b = InternalRoller(numdice, dicefaces, modifier);
+                    // Put each roll into the first two elements of the array.
+                    rollresult[0] = diceresult_a;
+                    rollresult[1] = diceresult_b;
 
-                    // Select the lower roll and return that. If neither is lower, return _a.
+                    // Find the lower roll, set third array element to denote desired roll; default to first roll if equal.
                     if (diceresult_a < diceresult_b)
                     {
-                        rollresult = diceresult_a.ToString();
+                        rollresult[2] = 0;
                         return rollresult;
                     }
                     else if (diceresult_b < diceresult_a)
                     {
-                        rollresult = diceresult_b.ToString();
+                        rollresult[2] = 1;
                         return rollresult;
                     }
                     else
                     {
-                        rollresult = diceresult_a.ToString();
+                        rollresult[2] = 0;
                         return rollresult;
                     }
                 }
                 default:
                 {
-                    // Default to a single roll.
+                    // Default to a single roll. 
                     int diceresult = InternalRoller(numdice, dicefaces, modifier);
-                    rollresult = diceresult.ToString();
+                    // Set first array element to the roll, leave second element as null, set third element to denote the first roll.
+                    rollresult[0] = diceresult;
                     return rollresult;
                 }
             }
@@ -105,10 +115,11 @@ namespace DiceOTron
       }
 
         // Output the results.
-        // Basic output for now; might get fancier later.
-        private static void Outputter(string rollermessage)
+        // Fixed output formatting for now.
+        // Note to self for later: add different output formatting based on an additional command-line argument?
+        private static void Outputter(int[] results)
         {
-            Console.WriteLine(rollermessage);       
+            // Code to output results goes here. I'm tired for now.
             return;
         }
     }
